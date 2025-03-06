d7eb300f2b7085467cc456ccc6829e70402f52fa
jasn-armstrng <jasnarmstrng@gmail.com>
2025-03-06 05:45:28 +0000

    refactor(utilities): Break out conversions into separate functions
    - Document each function
    - Update tests

--- utilities/src/main.rs
+++ utilities/src/main.rs
@@ -1,3 +1,6 @@
+use utilities::temperature::*;
+
 fn main() {
     println!("Hello, world!");
+    println!("100.0 C to F: {:.2}", celsius_to_fahrenheit(100.0));
 }
--- utilities/src/temperature.rs
+++ utilities/src/temperature.rs
@@ -1,78 +1,95 @@
-pub fn temperature_converter(value: f32, from: char, to: char) -> f32 {
-    // Utility:
-    //  - Convert between temperature units
-    // Inputs:
-    //  - value: The floating point value to be converted
-    //  - from: The unit the value is to be converted from. Case insensitive char - c/C, f/F, k/K for Celsius, Fahrenheit, and Kelvin respectively
-    //  - to:  he unit the value is to be converted to. ...
-    // Returns:
-    //  - A floating point value that is greater than or equal to the absolute-zero value for that resulting unit
+/// Temperature conversion suite
+const ABSOLUTE_ZERO_C: f32 = -273.15;
+const ABSOLUTE_ZERO_F: f32 = -459.67;
+const ABSOLUTE_ZERO_K: f32 = 0.0;
 
-    // If `from` eq `to`, return value
-    if from == to {
-        return value;
+/// Convert a celsius temperature to fahrenheit.
+///
+/// # Conversion formula
+/// (C * 9.0 / 5.0) + 32.0
+///
+/// # Return
+/// A floating point fahrenheit value that is greater than or equal to the absolute-zero (-459.67).
+pub fn celsius_to_fahrenheit(c: f32) -> f32 {
+    if c <= ABSOLUTE_ZERO_C {
+        ABSOLUTE_ZERO_F
+    } else {
+        (c * 9.0 / 5.0) + 32.0
     }
+}
 
-    // Match and return conversion for value
-    match (from.to_ascii_lowercase(), to.to_ascii_lowercase()) {
-        // Celsius to Fahrenheit
-        ('c', 'f') => {
-            // Compare conversion with absolute-zero before return
-            let result = (value * 9.0 / 5.0) + 32.0;
-            if result < -459.67 {
-                -459.67
-            } else {
-                result
-            }
-        }
-        // Fahrenheit to Celsius
-        ('f', 'c') => {
-            // Compare conversion with absolute-zero before return
-            let result = (value - 32.0) * 5.0 / 9.0;
-            if result < -273.15 {
-                -273.15
-            } else {
-                result
-            }
-        }
-        // Fahrenheit to Kelvin
-        ('f', 'k') => {
-            let result = (value - 32.0) * 5.0 / 9.0 + 273.15;
-            if result < 0.0 {
-                0.0
-            } else {
-                result
-            }
-        }
-        // Kelvin to Fahrenheit
-        ('k', 'f') => {
-            let result = value * 9.0 / 5.0 - 459.67;
-            if result < -459.67 {
-                -459.67
-            } else {
-                result
-            }
-        }
-        // Celsius to Kelvin
-        ('c', 'k') => {
-            let result = value + 273.15;
-            if result < 0.0 {
-                0.0
-            } else {
-                result
-            }
-        }
-        // Kelvin to Celsius
-        ('k', 'c') => {
-            let result = value - 273.15;
-            if result < -273.15 {
-                -273.15
-            } else {
-                result
-            }
-        }
-        // Return an absurd value for non-matches
-        _ => -999999.0,
+/// Convert a celsius temperature to kelvin.
+///
+/// # Conversion formula
+/// C + 273.15
+///
+/// # Return
+/// A floating point kelvin value that is greater than or equal to the absolute-zero (0.0).
+pub fn celsius_to_kelvin(c: f32) -> f32 {
+    if c <= ABSOLUTE_ZERO_C {
+        ABSOLUTE_ZERO_K
+    } else {
+        c + 273.15
+    }
+}
+
+/// Convert a fahrenheit temperature to celsius.
+///
+/// # Conversion formula
+/// (F - 32.0) * 5.0 / 9.0
+///
+/// # Return
+/// A floating point celsius value that is greater than or equal to the absolute-zero (-273.15).
+pub fn fahrenheit_to_celsius(f: f32) -> f32 {
+    if f <= ABSOLUTE_ZERO_F {
+        ABSOLUTE_ZERO_C
+    } else {
+        (f - 32.0) * 5.0 / 9.0
+    }
+}
+
+/// Convert a fahrenheit temperature to kelvin.
+///
+/// # Conversion formula
+/// (F - 32.0) * 5.0 / 9.0 + 273.15
+///
+/// # Return
+/// A floating point kelvin value that is greater than or equal to the absolute-zero (0.0).
+pub fn fahrenheit_to_kelvin(f: f32) -> f32 {
+    if f <= ABSOLUTE_ZERO_F {
+        ABSOLUTE_ZERO_K
+    } else {
+        (f - 32.0) * 5.0 / 9.0 + 273.15
+    }
+}
+
+/// Convert a kelvin temperature to celsius.
+///
+/// # Conversion formula
+/// K - 273.15
+///
+/// # Return
+/// A floating point celsius value that is greater than or equal to the absolute-zero (-273.15).
+pub fn kelvin_to_celsius(k: f32) -> f32 {
+    if k <= ABSOLUTE_ZERO_K {
+        ABSOLUTE_ZERO_C
+    } else {
+        k - 273.15
+    }
+}
+
+/// Convert a kelvin temperature to fahrenheit.
+///
+/// # Conversion formula
+/// K * 9.0 / 5.0 - 459.67
+///
+/// # Return
+/// A floating point fahrenheit celsius value that is greater than or equal to the absolute-zero (-459.67).
+pub fn kelvin_to_fahrenheit(k: f32) -> f32 {
+    if k <= ABSOLUTE_ZERO_K {
+        ABSOLUTE_ZERO_F
+    } else {
+        k * 9.0 / 5.0 - 459.67
     }
 }
 
@@ -97,68 +114,65 @@ mod tests {
 
     #[test]
     fn test_celsius_to_fahrenheit() {
-        assert_almost_eq(temperature_converter(0.0, 'c', 'f'), 32.0);
-        assert_almost_eq(temperature_converter(100.0, 'C', 'F'), 212.0);
-        assert_almost_eq(temperature_converter(-40.0, 'c', 'F'), -40.0);
+        assert_almost_eq(celsius_to_fahrenheit(ABSOLUTE_ZERO_C), ABSOLUTE_ZERO_F);
+        assert_almost_eq(
+            celsius_to_fahrenheit(ABSOLUTE_ZERO_C - 23.04),
+            ABSOLUTE_ZERO_F,
+        );
+        assert_almost_eq(celsius_to_fahrenheit(100.0), 212.0);
+        assert_almost_eq(celsius_to_fahrenheit(55.0), 131.0);
     }
 
     #[test]
     fn test_fahrenheit_to_celsius() {
-        assert_almost_eq(temperature_converter(32.0, 'f', 'c'), 0.0);
-        assert_almost_eq(temperature_converter(212.0, 'F', 'C'), 100.0);
-        assert_almost_eq(temperature_converter(-40.0, 'F', 'c'), -40.0);
+        assert_almost_eq(fahrenheit_to_celsius(ABSOLUTE_ZERO_F), ABSOLUTE_ZERO_C);
+        assert_almost_eq(
+            fahrenheit_to_celsius(ABSOLUTE_ZERO_F - 16.0),
+            ABSOLUTE_ZERO_C,
+        );
+        assert_almost_eq(fahrenheit_to_celsius(212.0), 100.0);
+        assert_almost_eq(fahrenheit_to_celsius(131.0), 55.0);
     }
 
     #[test]
     fn test_celsius_to_kelvin() {
-        assert_almost_eq(temperature_converter(0.0, 'c', 'k'), 273.15);
-        assert_almost_eq(temperature_converter(-273.15, 'C', 'K'), 0.0);
-        assert_almost_eq(temperature_converter(100.0, 'c', 'K'), 373.15);
+        assert_almost_eq(celsius_to_kelvin(ABSOLUTE_ZERO_C), ABSOLUTE_ZERO_K);
+        assert_almost_eq(celsius_to_kelvin(ABSOLUTE_ZERO_C - 9.222), ABSOLUTE_ZERO_K);
+        assert_almost_eq(celsius_to_kelvin(0.0), 273.15);
+        assert_almost_eq(celsius_to_kelvin(-273.15), 0.0);
+        assert_almost_eq(celsius_to_kelvin(100.0), 373.15);
     }
 
     #[test]
     fn test_kelvin_to_celsius() {
-        assert_almost_eq(temperature_converter(273.15, 'k', 'c'), 0.0);
-        assert_almost_eq(temperature_converter(0.0, 'K', 'C'), -273.15);
-        assert_almost_eq(temperature_converter(373.15, 'K', 'c'), 100.0);
+        assert_almost_eq(kelvin_to_celsius(ABSOLUTE_ZERO_K), ABSOLUTE_ZERO_C);
+        assert_almost_eq(kelvin_to_celsius(ABSOLUTE_ZERO_K - 889.0), ABSOLUTE_ZERO_C);
+        assert_almost_eq(kelvin_to_celsius(273.15), 0.0);
+        assert_almost_eq(kelvin_to_celsius(0.0), -273.15);
+        assert_almost_eq(kelvin_to_celsius(373.15), 100.0);
     }
 
     #[test]
     fn test_fahrenheit_to_kelvin() {
-        assert_almost_eq(temperature_converter(32.0, 'f', 'k'), 273.15);
-        assert_almost_eq(temperature_converter(-459.67, 'F', 'K'), 0.0);
-        assert_almost_eq(temperature_converter(212.0, 'f', 'K'), 373.15);
+        assert_almost_eq(fahrenheit_to_kelvin(ABSOLUTE_ZERO_F), ABSOLUTE_ZERO_K);
+        assert_almost_eq(
+            fahrenheit_to_kelvin(ABSOLUTE_ZERO_F - 10.9),
+            ABSOLUTE_ZERO_K,
+        );
+        assert_almost_eq(fahrenheit_to_kelvin(32.0), 273.15);
+        assert_almost_eq(fahrenheit_to_kelvin(-459.67), 0.0);
+        assert_almost_eq(fahrenheit_to_kelvin(212.0), 373.15);
     }
 
     #[test]
     fn test_kelvin_to_fahrenheit() {
-        assert_almost_eq(temperature_converter(273.15, 'k', 'f'), 32.0);
-        assert_almost_eq(temperature_converter(0.0, 'K', 'F'), -459.67);
-        assert_almost_eq(temperature_converter(373.15, 'K', 'F'), 212.0);
-    }
-
-    #[test]
-    fn test_same_unit_conversion() {
-        assert_almost_eq(temperature_converter(42.0, 'c', 'c'), 42.0);
-        assert_almost_eq(temperature_converter(42.0, 'f', 'f'), 42.0);
-        assert_almost_eq(temperature_converter(42.0, 'k', 'k'), 42.0);
-    }
-
-    #[test]
-    fn test_case_insensitivity() {
-        // Test that function handles uppercase and lowercase units the same
-        let temp = 42.0;
+        assert_almost_eq(kelvin_to_fahrenheit(ABSOLUTE_ZERO_K), ABSOLUTE_ZERO_F);
         assert_almost_eq(
-            temperature_converter(temp, 'c', 'f'),
-            temperature_converter(temp, 'C', 'F'),
+            kelvin_to_fahrenheit(ABSOLUTE_ZERO_K - 93.3),
+            ABSOLUTE_ZERO_F,
         );
-        assert_almost_eq(
-            temperature_converter(temp, 'f', 'k'),
-            temperature_converter(temp, 'F', 'K'),
-        );
-        assert_almost_eq(
-            temperature_converter(temp, 'k', 'c'),
-            temperature_converter(temp, 'K', 'C'),
-        );
+        assert_almost_eq(kelvin_to_fahrenheit(273.15), 32.0);
+        assert_almost_eq(kelvin_to_fahrenheit(0.0), -459.67);
+        assert_almost_eq(kelvin_to_fahrenheit(373.15), 212.0);
     }
 }
